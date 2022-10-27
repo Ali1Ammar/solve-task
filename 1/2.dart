f51ec34f0f0fb62ffbd958077b418910d704fee9
Ali Ammar <aliammarali125@gmail.com>
2022-10-27 20:38:23 +0300

    solve 2

---
+++ 1/2.dart
@@ -0,0 +1,34 @@
+/*
+complete the below code using relational and arithmetic operator
+ */
+
+void main(List<String> args) {
+  int x = 5;
+  int y = 10;
+
+  print("is x equal y:");
+  print();
+
+  print("is x bigger than y:");
+  print();
+
+  print("is x not equal y:");
+  print();
+
+  print("is x less than or equal y:");
+  print();
+
+  print("the value of 5 plus 2 is");
+  print();
+
+  print("the value of 3 multiply 6 is");
+  print();
+
+  print("the value of 5 divide by 2 is");
+  print();
+
+  print("the integer value of 5 divide by 2 is");
+  print();
+
+  print("is x between 10 and 30");
+  print
