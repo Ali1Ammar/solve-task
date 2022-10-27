2736c267731b4ec49175157931aeada6178a02eb
Ali Ammar <aliammarali125@gmail.com>
2022-10-27 20:37:20 +0300

    solve 1

---
+++ 1/1.dart
@@ -0,0 +1,13 @@
+/*
+write a variable in each of below value type and print its value (try to not use 'var/final/const' keyword)
+the final output may be like this
+true
+4
+1.2
+ok
+[4,3,"ok",true]
+ */
+
+void main(List<String> args) {
+  print("ok");
+}
